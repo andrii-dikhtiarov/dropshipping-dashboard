d6b3b5904ff3edae5ad537bd32db5ec53e3d9fcf
andriid <andrey.dihtiarov@gmail.com>
2025-02-13 15:41:34 +0200

    chore: move to components folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { Header } from './components/header';
+import { Header } from '@/components/header';
 import type React from 'react';
 import { Toaster } from '@/components/ui/toaster';
 

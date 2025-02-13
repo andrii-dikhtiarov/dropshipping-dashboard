6b000560dca97497365672a52ff6be6e31509b2f
andriid <andrey.dihtiarov@gmail.com>
2025-02-13 12:21:00 +0200

    fix: add use client to resolve server sturtup issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts"
